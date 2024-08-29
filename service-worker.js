42b2455fb5815e5fbac077b299999cffbadc26dc
Geimisson Romário <geimisson@gmail.com>
2024-08-29 13:04:59 -0300

    Adicionando icone e colocando arquijo .jason para instalar

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,26 @@
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('expense-cache').then((cache) => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/styles.css',
-          '/app.js',
-          '/manifest.json',
-          '/icon-192.png',
-          '/icon-512.png',
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+const CACHE_NAME = 'expense-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  '/manifest.json',
+  '/icons/icone.png',
+  '/icons/icone.png',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
